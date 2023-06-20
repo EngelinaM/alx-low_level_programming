c68a6b48f2b17293e33358134ddb77f5b33cd452
EngelinaM <molepome2@gmail.com>
2023-06-20 01:01:25 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase with proper grammar,
- * but the outcome is a piece of art,
+ * main - Entry point
+ * 
  * Return: Always 0(Success)
  */
 int main (void)
